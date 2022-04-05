64849530631aadf8f0ca231bffcf7f3f7d6dc0bf
yuyu56789 <yuxiliu2021@u.northwestern.edu>
2022-04-04 20:46:18 -0500

    beginDrawing00

---
+++ beginDrawing/DotsAndLines.js
@@ -0,0 +1,134 @@
+//3456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_
+// (JT: why the numbers? counts columns, helps me keep 80-char-wide listings)
+//
+// MultiPoint.js (c) 2012 matsuda
+// MultiPointJT.js  MODIFIED for EECS 351-1, Northwestern Univ. Jack Tumblin
+//						(converted to 2D->4D; 3 verts --> 6 verts; draw as
+//						gl.POINTS and as gl.LINE_LOOP, change color.
+//
+// Vertex shader program.  
+//  Each instance computes all the on-screen attributes for just one VERTEX,
+//  specifying that vertex so that it can be used as part of a drawing primitive
+//  depicted in the CVV coord. system (+/-1, +/-1, +/-1) that fills our HTML5
+//  'canvas' object.
+// Each time the shader program runs it gets info for just one vertex from our 
+//	Vertex Buffer Object (VBO); specifically, the value of its 'attribute' 
+// variable a_Position, is supplied by the VBO.
+// 
+//   CHALLENGE: Change the program to get different pictures. 
+//	See if you can:
+//	EASY:
+//    --change the background color?
+//		--change the dot positions? 
+//		--change the size of the dots?
+//    --change the color of the dots-and-lines?
+//	HARDER: (HINT: read about 'uniform' vars in your textbook...)
+//    --change the number of dots?
+//    --get all dots in one color, and all lines in another color?
+//    --set each dot color individually? (what happens to the line colors?)
+//
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
+  '}\n';
+
+// Fragment shader program
+//  Each instance computes all the on-screen attributes for just one PIXEL
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n' +
+  '}\n';
+
+function main() {
+//==============================================================================
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Write buffer full of vertices to the GPU, and make it available to shaders
+  var n = initVertexBuffers(gl);	
+  if (n < 0) {
+    console.log('Failed to load vertices into the GPU');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>: (Northwestern purple)
+  gl.clearColor(78/255, 42/255, 132/255 , 1.0);	// R,G,B,A (A==opacity)
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw connect-the-dots for 6 vertices (never 'vertexes'!!).
+  // see http://www.khronos.org/opengles/sdk/docs/man/xhtml/glDrawArrays.xml
+ gl.drawArrays(gl.LINE_LOOP, 0, n); // gl.drawArrays(mode, first, count)
+			//mode: sets drawing primitive to use. Other valid choices: 
+				// gl.LINES, gl.LINE_STRIP, gl.LINE_LOOP, 
+				// gl.TRIANGLES, gl.TRIANGLES_STRIP, gl.TRIANGLE_FAN
+			// first: index of 1st element of array.
+			// count; number of elements to read from the array.
+
+  // That went well. Let's draw the dots themselves!
+  gl.drawArrays(gl.POINTS, 0, n); // gl.drawArrays(mode, first, count)
+			//mode: sets drawing primitive to use. Other valid choices: 
+				// gl.LINES, gl.LINE_STRIP, gl.LINE_LOOP, 
+				// gl.TRIANGLES, gl.TRIANGLES_STRIP, gl.TRIANGLE_FAN
+			// first: index of 1st element of array.
+			// count; number of elements to read from the array.
+}
+
+
+function initVertexBuffers(gl) {
+//==============================================================================
+// first, create an array with all our vertex attribute values:
+  var vertices = new Float32Array([
+     0.0,  0.5, 0.0, 1.0,	// CAREFUL! I made these into 4D points/ vertices: x,y,z,w.
+    -0.2,  0.0, 0.0, 1.0,	// new point!  (? What happens if I make w=0 instead of 1.0?)
+    -0.5, -0.5, 0.0, 1.0,   
+     0.0, -0.2, 0.0, 1.0, 	// new point!
+     0.5, -0.5, 0.0, 1.0,	// 
+     0.2,  0.0, 0.0, 1.0, 	// new point!  (note we need a trailing comma here)
+     
+  ]);
+  var n = 6; // The number of vertices
+
+  // Then in the Graphics hardware, create a vertex buffer object (VBO)
+  var vertexBuffer = gl.createBuffer();	// get it's 'handle'
+  if (!vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // COPY data from our 'vertices' array into the vertex buffer object in GPU:
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var a_PositionID = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_PositionID < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_PositionID, 4, gl.FLOAT, false, 0, 0);
+  // vertexAttributePointer(index, x,y,z,w size=4, type=FLOAT, 
+  // NOT normalized, NO stride)
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_PositionID);
+
+  return n;
+}
